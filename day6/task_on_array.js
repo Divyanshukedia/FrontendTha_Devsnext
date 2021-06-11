1bc38db0fa1803c47883dc233720e4523263d806
Anchal Singh <83942897+anchalsinghrajput@users.noreply.github.com>
2021-06-11 17:23:27 +0530

    Update task_on_array.js

--- day6/task_on_array.js
+++ day6/task_on_array.js
@@ -1,1 +1,17 @@
+//*Question1
 
+var is_array = function(input) {
+  if (toString.call(input) === "[object Array]")
+    return true;
+  return false;   
+    };
+console.log(is_array('w3resource'));
+console.log(is_array([1, 2, 4, 0]));
+
+//*Question2
+
+var array_Clone = function(arra1) {
+ return arra1.slice(0);
+    };
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]]));
