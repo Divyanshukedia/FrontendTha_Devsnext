2a461df367bcbfe9b136adf3b562935707eb8efc
Anchal Singh <83942897+anchalsinghrajput@users.noreply.github.com>
2021-06-12 20:00:03 +0530

    Update task_on_array.js

--- day6/task_on_array.js
+++ day6/task_on_array.js
@@ -16,6 +16,19 @@ var array_Clone = function(arra1) {
 console.log(array_Clone([1, 2, 4, 0]));
 console.log(array_Clone([1, 2, [4, 0]]));
 
+//Question 3
+
+var first =  function(arr, n) {
+      if (arr == null) 
+      return 0;
+    if (n == null) 
+      return arr[0];
+    if (n < 0)
+      return [];
+    return arr.slice(0, n);
+};
+console.log(first([7, 9, 0, -2]));
+
 //Question4
 
 myColor = ["Red", "Green", "White", "Black"];
