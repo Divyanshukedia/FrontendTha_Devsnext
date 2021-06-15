646a1b9193a7c471fe97612080064669d3725128
Anchal Singh <83942897+anchalsinghrajput@users.noreply.github.com>
2021-06-15 23:15:13 +0530

    Update object.js

--- day7/object.js
+++ day7/object.js
@@ -1,14 +1,139 @@
-Code:
-var student = {
-    name: "David Rayy",
-    sclass: "VI",
-    rollno: 12
-  };
+//question 1
 
-  let result = "";
+var student = 
+{ 
+  name : "David Rayy", 
+  sclass : "VI", 
+  rollno : 12 
+}
 
-  for (let key in student)
-    result += `${key},`;
+var a = "";
 
-  console.log(result.substring(0, result.length - 1));
+  for (const key in student)
+    a=a+ `${key},`;
+
+  console.log(a.substring(0, a.length - 1));
+
+console.log("");
+
+// question 2
+
+var student = 
+{ 
+  name : "David Rayy", 
+  sclass : "VI", 
+  rollno : 12 
+}
+
+console.log(student);
+delete student.rollno;
+console.log(student);
+
+
+// question 3
+
+var student = 
+{ 
+  name : "David Rayy", 
+  sclass : "VI", 
+  rollno : 12 
+}
+var size = Object.values(student).length;
+console.log(size);
+
+
+// question 4
+
+var library = 
+[
+  { 
+      author: 'Bill Gates', 
+      title: 'The Road Ahead', 
+      readingStatus: true 
+    
+  }, 
+      
+  {   
+      author: 'Steve Jobs', 
+      title: 'Walter Isaacson', 
+      readingStatus: true 
+    
+  }, 
   
+  {   
+      author: 'Suzanne Collins', 
+      title: 'Mockingjay: The Final Book of The Hunger Games', 
+      readingStatus: false 
+    
+  }
+]; 
+
+
+// console.log(library[0].title);
+for(i=0;i<library.length;i++)
+{
+    
+    console.log("book name --- "+library[i].title);
+    console.log("author name --- "+library[i].author);
+    console.log("reading status --- "+library[i].readingStatus);
+  
+    console.log("");
+}
+
+// question 5
+
+var Cylinder = class {
+  constructor(height, radius) {
+    this.height = height;
+    this.radius = radius;
+  }
+
+  get volume() {
+    return this.calcVolume();
+  }
+
+  calcVolume() {
+    return (Math.PI*this.radius*this.radius*this.height);
+  }
+
+};
+
+const obj = new Cylinder(7,2);
+console.log("volume is ", obj.volume.toFixed(4));
+
+// question 6
+
+var library = [ 
+  
+    { 
+      title: 'The Road Ahead', 
+      author: 'Bill Gates', 
+      libraryID: 1254 
+      
+    }, 
+    { 
+      title: 'Walter Isaacson', 
+      author: 'Steve Jobs', 
+      libraryID: 4264 
+      
+    }, 
+    { 
+      title: 'Mockingjay: The Final Book of The Hunger Games', 
+      author: 'Suzanne Collins', 
+      libraryID: 3245 
+      
+    }
+  ];
+
+function compare_to_sort(x,y) 
+ {
+  if (x.libraryID < y.libraryID)
+    return 1;
+  if (x.libraryID > y.libraryID)
+    return -1;
+  return 0;
+ }
+ 
+
+library.sort(compare_to_sort);
+console.log(library);
